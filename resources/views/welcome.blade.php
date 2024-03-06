92aeda100d7dbc8eb1f66539fad0e8b985a52316
yaseen <yasikalleri@gmail.com>
2024-03-06 14:46:55 +0530

    final , added authors name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,9 @@
 @section('content')
 <div class="container">
     <div class="d-flex justify-content-between">
-    <h3 class="mb-3 mt-3">Blog Posts</h3>
-    <a href="{{ route('blogs') }}" class="btn btn-success my-3">New Post</a>
-</div>
+        <h3 class="mb-3 mt-3">Blog Posts</h3>
+        <a href="{{ route('blogs') }}" class="btn btn-success my-3">New Post</a>
+    </div>
     <div class="row">
         @foreach ($blogPosts as $post)
         <div class="col-md-4 mb-4">
@@ -12,15 +12,14 @@
                 <div class="card-body">
                     <h5 class="card-title">{{$post->title}}</h5>
                     <p class="card-text">{{$post->description}}</p>
-                    <a href="" class="btn btn-primary">Edit</a>
-                    <a href="" class="btn btn-danger">Delete</a>
+                    <p class="card-text"><strong>Author:</strong> {{$post->user->name}}</p>
+                    <!-- You can display other user information like email, etc. -->
                 </div>
             </div>
         </div>
         @endforeach
     </div>
     <div class="mt-4">
-
     </div>
 </div>
 {{session()->get('user_name')}}
